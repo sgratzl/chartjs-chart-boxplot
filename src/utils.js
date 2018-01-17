43086795e0d31f4e8d713884c020014c011966f5
sluger <stefan.luger@datavisyn.io>
2018-01-17 10:15:04 +0100

    satisfy eslint

--- src/utils.js
+++ src/utils.js
@@ -1,14 +1,14 @@
 'use strict';
 
 export function computeLaneWidth(width, padding) {
-    if (padding == null || padding === 0) {
-        return width;
-    }
-    let laneWidth;
-    if (padding > 1) { // compute as pixel
-        laneWidth = width - Math.abs(padding);
-    } else { // compute as percent
-        laneWidth = width - width * Math.abs(padding);
-    }
-    return laneWidth < 0 ? 0 : laneWidth;
+	if (padding === null || !padding || padding === 0) {
+		return width;
+	}
+	let laneWidth;
+	if (padding > 1) { // compute as pixel
+		laneWidth = width - Math.abs(padding);
+	} else { // compute as percent
+		laneWidth = width - width * Math.abs(padding);
+	}
+	return laneWidth < 0 ? 0 : laneWidth;
 }
