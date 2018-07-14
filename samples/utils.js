1d8956e1464fa772ac41cb4cf5e94dd8eaf62d2a
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-07-14 13:28:42 +0200

    fix random generator

--- samples/utils.js
+++ samples/utils.js
@@ -48,11 +48,12 @@ window.chartColors = {
     },
 
     randF: function(min, max) {
-      var seed = this._seed;
       min = min === undefined ? 0 : min;
       max = max === undefined ? 1 : max;
-      this._seed = (seed * 9301 + 49297) % 233280;
-      return () => min + (this._seed / 233280) * (max - min);
+      return () => {
+        this._seed = (this._seed * 9301 + 49297) % 233280;
+        return min + (this._seed / 233280) * (max - min);
+      };
     },
 
     rand: function(min, max) {
