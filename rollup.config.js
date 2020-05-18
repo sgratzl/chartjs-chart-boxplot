e3d73f95e44800ee52473561b07153f1a3daa8dd
Samuel Gratzl <sam@sgratzl.com>
2020-05-18 10:50:52 +0200

    update setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,34 @@
 // rollup.config.js
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import babel from 'rollup-plugin-babel';
+import pnp from 'rollup-plugin-pnp-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
 
 export default [
   {
+    input: 'src/bundle.js',
     output: {
-      file: 'build/Chart.BoxPlot.js',
-      name: 'ChartBoxPlot',
+      file: 'build/Chart.Geo.js',
+      name: 'ChartGeo',
       format: 'umd',
       globals: {
         'chart.js': 'Chart',
       },
     },
     external: ['chart.js'],
-    plugins: [resolve(), commonjs(), babel()],
+    plugins: [commonjs(), pnp(), resolve(), babel({ babelHelpers: 'runtime' })],
   },
   {
+    input: 'src/index.js',
     output: {
-      file: 'build/Chart.BoxPlot.esm.js',
-      name: 'ChartBoxPlot',
+      file: 'build/Chart.Geo.esm.js',
+      name: 'ChartGeo',
       format: 'esm',
       globals: {
         'chart.js': 'Chart',
       },
     },
-    external: ['chart.js'],
-    plugins: [resolve(), commonjs(), babel()],
+    external: ['chart.js', '@babel/runtime', 'd3-geo', 'd3-scale-chromatic', 'topojson-client'],
+    plugins: [commonjs(), pnp(), resolve()],
   },
 ];
