014278562081aeed93b0bd24ccc748d6434f7370
Samuel Gratzl <sam@sgratzl.com>
2022-02-18 09:18:07 -0500

    feat: add violin example

--- samples/type_test.ts
+++ samples/type_test.ts
@@ -1,5 +1,5 @@
 import { Chart, LinearScale, CategoryScale } from 'chart.js';
-import { BoxPlotController, BoxAndWiskers } from '../build';
+import { BoxPlotController, BoxAndWiskers, Violin, ViolinController } from '../build';
 
 // register controller in chart.js and ensure the defaults are set
 Chart.register(BoxPlotController, BoxAndWiskers, LinearScale, CategoryScale);
@@ -30,6 +30,36 @@ const myBar = new Chart(ctx, {
   options: {
     elements: {
       boxplot: {
+        outlierRadius: 10,
+      },
+    },
+  },
+});
+
+const myViolin = new Chart(ctx, {
+  type: 'violin',
+  data: {
+    labels: ['vs'],
+    datasets: [
+      {
+        label: 'Dataset 1',
+        outlierRadius: 10,
+        data: [
+          [1, 2, 3, 4, 5],
+          {
+            min: 1,
+            q1: 2,
+            median: 3,
+            q3: 4,
+            max: 5,
+          },
+        ],
+      },
+    ],
+  },
+  options: {
+    elements: {
+      violin: {
         outlierRadius: 10,
       },
     },
