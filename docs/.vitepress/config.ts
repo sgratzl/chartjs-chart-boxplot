8dc9ebfaca28af149181c5935d0ee35db992aeb3
Samuel Gratzl <sam@sgratzl.com>
2023-07-03 17:31:12 -0400

    build: generate typedoc

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitepress';
 import {name, description, repository, license, author } from '../../package.json';
+import typedocSidebar from '../api/typedoc-sidebar.json';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Examples', link: '/markdown-examples' },
+      { text: 'API', link: '/api/' },
     ],
 
     sidebar: [
@@ -22,23 +24,28 @@ export default defineConfig({
           { text: 'Runtime API Examples', link: '/api-examples' },
         ],
       },
+      {
+        text: 'API',
+        items: typedocSidebar,
+      },
     ],
 
-    socialLinks: [
-      { icon: 'github', link: repository.url.replace('.git', '') }
-    ],
+    socialLinks: [{ icon: 'github', link: repository.url.replace('.git', '') }],
 
     footer: {
-      message: `Released under the <a href="${repository.url.replace('.git', '')}/tree/main/LICENSE">${license} license</a>.`,
-      copyright: `Copyright © 2019-present <a href="${author.url}">${author.name}</a>`
+      message: `Released under the <a href="${repository.url.replace(
+        '.git',
+        ''
+      )}/tree/main/LICENSE">${license} license</a>.`,
+      copyright: `Copyright © 2019-present <a href="${author.url}">${author.name}</a>`,
     },
 
     editLink: {
-      pattern: `${repository.url.replace('.git', '')}/edit/main/docs/:path`
+      pattern: `${repository.url.replace('.git', '')}/edit/main/docs/:path`,
     },
 
     search: {
-      provider: 'local'
-    }
+      provider: 'local',
+    },
   },
 });
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
 export * from './elements';
 export * from './controllers';
+
+export type {
+  IBaseOptions,
+  IBaseStats,
+  IBoxPlot,
+  IBoxplotOptions,
+  IKDEPoint,
+  IViolin,
+  IViolinOptions,
+  QuantileMethod,
+} from './data';
+
+export type { ExtendedTooltip } from './tooltip';
